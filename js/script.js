b8e4ccd012b382e8e5565c6dbfa889c18d3bca07
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-09-25 16:28:09 +0200

    [Add]:  other posts, change date and like

--- js/script.js
+++ js/script.js
@@ -55,3 +55,79 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+function newPost(post) {
+  return `<div class="post">
+    <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img
+            class="profile-pic"
+            src=${post.author.image}
+            alt=${post.author.name}
+          />
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${post.author.name}</div>
+          <div class="post-meta__time">${post.created}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">
+      ${post.content}
+    </div>
+    <div class="post__image">
+      <img src=${post.media} alt="" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <a class="like-button js-like-button" href="#" data-postid="1">
+            <i
+              class="like-button__icon fas fa-thumbs-up"
+              aria-hidden="true"
+            ></i>
+            <span class="like-button__label">Mi Piace</span>
+          </a>
+        </div>
+        <div class="likes__counter">
+          Piace a
+          <b id="like-counter-1" class="js-likes-counter">
+            ${post.likes}
+          </b>
+          persone
+        </div>
+      </div>
+    </div>
+  </div>`;
+}
+
+
+//querySelectors
+const container = document.querySelector('#container');
+
+for (let i = 0; i < posts.length; i++) {
+  //manipolazioni date
+  posts[i].created = posts[i].created.split('-').reverse().join('/');
+  container.innerHTML += newPost(posts[i]);
+}
+
+//aggiunta eventi ai like buttons
+const likedID = [];
+const likeButtons = document.querySelectorAll('.like-button');
+for (let i = 0; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener('click', function () {
+    //aggiunta classe like ai button e link all'id nel array likeID
+    likeButtons[i].classList.add('like-button--liked');
+
+
+    //aumneta i like nell' oggetto e nel html
+    posts[i].likes++;
+    document.querySelectorAll('#like-counter-1')[i].innerHTML = posts[i].likes;
+
+    likedID.push(posts[i].id);
+    console.log(likedID);
+  });
+}
